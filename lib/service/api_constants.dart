66016ee3029cdb24fd6f463a14a28cbce0b79270
Phuong <phuongphuong262204@gmail.com>
2024-11-05 08:21:41 +0700

    add api to app

---
+++ lib/service/api_constants.dart
@@ -0,0 +1,10 @@
+class ApiConstants {
+  static const String baseUrl = "http://localhost:8080/api/v1"; 
+
+  static const String registerUser = "/auth/user/signup";
+  static const String loginUser = "/auth/user/signip";
+  static const String changePassword = "/user/change-password";
+
+  static const String profile = "/user/profile";
+  static const String updateProfile = "/user/update-profile";
+}
